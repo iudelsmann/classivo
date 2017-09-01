fabf03562c792ca2e1e326f4beb1f949b9474269
Ivo Udelsmann <iudelsmann@gmail.com>
2017-08-31 21:56:28 -0300

    Fixes to circleci deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyCIFsxMVbQn55sT7-kT0dS43OrziGUDwP8',
+    authDomain: 'classivo-one.firebaseapp.com',
+    databaseURL: 'https://classivo-one.firebaseio.com',
+    projectId: 'classivo-one',
+    storageBucket: 'classivo-one.appspot.com',
+    messagingSenderId: '766719720224',
+  },
 };
