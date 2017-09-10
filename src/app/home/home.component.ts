f175b9d008c52aed76b78d8311dadb1c21351955
Ivo Udelsmann <iudelsmann@gmail.com>
2017-09-10 12:36:24 -0300

    Fixing layout and adding add fab

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
+import { Class } from '../model/class';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  classes: FirebaseListObservable<any>;
+  classes: FirebaseListObservable<Class[]>;
 
   constructor(db: AngularFireDatabase) {
     this.classes = db.list('/classes');
---
+++ src/app/model/class.ts
@@ -0,0 +1,15 @@
+/**
+ * Interface to represent a Class (as in a course).
+ *
+ * @export
+ * @interface Class
+ */
+export interface Class {
+  /**
+   * The name of class.
+   *
+   * @type {string}
+   * @memberof Class
+   */
+  name: string;
+}
